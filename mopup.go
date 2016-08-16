72a38f7bad82d97496feab3d6f77cd9708e97738
Sudarsan <sudar.theone@gmail.com>
2016-08-16 19:21:18 +0530

    removeStopwords is a bit buggy

--- mopup.go
+++ mopup.go
@@ -7,10 +7,24 @@ import (
 
 func Mopup(corpus []string) []string{
 	re := regexp.MustCompile("[0-9!-<>:@?/(),\"]+")
+	stopWords := ReadFiles([]string{"stopwords.txt"})
 	for i , data := range corpus{
 		corpus[i] = strings.ToLower(data)
 		corpus[i] = re.ReplaceAllString(corpus[i], "")
+		corpus[i] = RemoveStopWords(corpus[i] , stopWords)
 	}
 	return corpus
+}
+
+func RemoveStopWords(line string, stopWords []string) string{
+	regStr := ""
+	for i, word := range stopWords{
+		if i != 0{
+			regStr += `|`
+		}
+		regStr += `\Q` +  word +  `\E`
+	}
+	re := regexp.MustCompile(regStr)
+	return re.ReplaceAllString(line, " ")
 }
 
--- test/test.go
+++ test/test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main(){
-	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1"
+	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1\\test"
 	fileList := glub.GetFiles(path)
 	content := glub.ReadFiles(fileList)
 	content = glub.Mopup(content)
