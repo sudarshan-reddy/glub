cbae97459f7c90ffdc7d6b74208a644df10601a5
Sudarsan <sudar.theone@gmail.com>
2016-08-16 18:40:53 +0530

    added mopup

---
+++ mopup.go
@@ -0,0 +1,17 @@
+package glub
+
+import (
+	"strings"
+	"regexp"
+	"fmt"
+)
+
+func Mopup(corpus []string) []string{
+	re := regexp.MustCompile("[0-9]+")
+	for i , data := range corpus{
+		corpus[i] = strings.ToLower(data)
+		corpus[i] = re.ReplaceAllString(corpus[i], "")
+	}
+	return corpus
+}
+
--- test/test.go
+++ test/test.go
@@ -9,5 +9,7 @@ func main(){
 	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1"
 	fileList := glub.GetFiles(path)
 	content := glub.ReadFiles(fileList)
+	content = glub.Mopup(content)
 	fmt.Println(content)
+
 }
