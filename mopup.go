56dc381da612e6d409df5050d65246ecfb560e2e
Sudarsan <sudar.theone@gmail.com>
2016-08-17 12:23:00 +0530

    fixed the regex bug with stopwords

--- mopup.go
+++ mopup.go
@@ -8,6 +8,7 @@ import (
 func Mopup(corpus []string) []string{
 	re := regexp.MustCompile("[0-9!-<>:@?/(),\"]+")
 	stopWords := ReadFiles([]string{"stopwords.txt"})
+	stopWords = strings.Split(stopWords[0], ",")
 	for i , data := range corpus{
 		corpus[i] = strings.ToLower(data)
 		corpus[i] = re.ReplaceAllString(corpus[i], "")
@@ -18,13 +19,14 @@ func Mopup(corpus []string) []string{
 
 func RemoveStopWords(line string, stopWords []string) string{
 	regStr := ""
+
 	for i, word := range stopWords{
 		if i != 0{
 			regStr += `|`
 		}
-		regStr += `\Q` +  word +  `\E`
+		regStr += `\b\Q` +  word +  `\E\b`
 	}
 	re := regexp.MustCompile(regStr)
-	return re.ReplaceAllString(line, " ")
+	return re.ReplaceAllString(line, "")
 }
 
