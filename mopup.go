2cad67daba6366666301d40d4be73207a1bbf046
Sudarsan <sudar.theone@gmail.com>
2016-08-22 15:15:03 +0530

    basic parallelization on mopup

--- mopup.go
+++ mopup.go
@@ -3,19 +3,43 @@ package glub
 import (
 	"strings"
 	"regexp"
+	"runtime"
+	"sync"
+	"fmt" //Temporary status logs
 )
 
 func Mopup(corpus []string) []string{
-	re := regexp.MustCompile("[0-9!-<>:@?/(),\"]+")
+
+	corpLen := len(corpus)
+
+	runtime.GOMAXPROCS(8)
+	var wg sync.WaitGroup
+	wg.Add(corpLen)	
+	
 	stopWords := ReadFiles([]string{"stopwords.txt"})
 	stopWords = strings.Split(stopWords[0], ",")
-	for i , data := range corpus{
-		corpus[i] = strings.ToLower(data)
-		corpus[i] = re.ReplaceAllString(corpus[i], "")
-		corpus[i] = RemoveStopWords(corpus[i] , stopWords)
-		corpus[i] = RemoveExtraWhiteSpaces(corpus[i])
+	s := make([]string, corpLen)
+	mop1 := mop{corpus: s, stopWords : stopWords}
+	for i, data := range corpus{
+		go mop1.mopupSlv(&wg, i, data)	
 	}
-	return corpus
+	wg.Wait()
+	return mop1.corpus
+}
+
+type mop struct{
+	corpus []string
+	stopWords []string
+}
+
+func (m *mop) mopupSlv(wg *sync.WaitGroup, i int, data string) {
+	fmt.Println("Starting " , i)
+	defer wg.Done()
+	re := regexp.MustCompile("[0-9!-<>:@?/(),\"]+")
+	m.corpus[i] = strings.ToLower(data)
+	m.corpus[i] = re.ReplaceAllString(m.corpus[i], "")
+	m.corpus[i] = RemoveStopWords(m.corpus[i] , m.stopWords)
+	m.corpus[i] = RemoveExtraWhiteSpaces(m.corpus[i])
 }
 
 func RemoveStopWords(line string, stopWords []string) string{
--- test/test.go
+++ test/test.go
@@ -3,18 +3,23 @@ package main
 import (
 	"glub"
 	"fmt"
+	"time"
 )
 
 func main(){
 	fmt.Println("test")
+	start := time.Now()
 	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1"
 	fileList := glub.GetFiles(path)
 	content := glub.ReadFiles(fileList)
+	fmt.Println("mopping up")
 	content = glub.Mopup(content)
 	tokens := make(map[string]int)
 	tokens = glub.GenTokens(content, tokens)
 	for k, v := range tokens{
 		fmt.Println(k, v)
 	}
+	elapsed := time.Since(start)
+	fmt.Println("time taken: ",  elapsed)
 
 }
