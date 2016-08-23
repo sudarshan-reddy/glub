81b033732155d5b6d7382d4ecd830b7e89e6a637
Sudarsan <sudar.theone@gmail.com>
2016-08-23 16:41:27 +0530

    intermediate data prep changes

--- test/test.go
+++ test/test.go
@@ -9,13 +9,13 @@ import (
 func main(){
 	fmt.Println("test")
 	start := time.Now()
-	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1\\test"
+	path := "C:\\Users\\sudarsang\\Downloads\\enron1\\enron1\\enron1"
 	fileList := glub.GetFiles(path)
 	content := glub.ReadFiles(fileList)
-	dataset := glub.GenTokens(content)
-	
-	for k, v := range dataset{
-		fmt.Println(k, v)
+	allTokens, metadata := glub.GenTokens(content)
+	dataset := glub.Prep(allTokens, metadata)
+	for keys, vals := range dataset{
+		fmt.Println(keys, vals)
 	}
 	elapsed := time.Since(start)
 	fmt.Println("time taken: ",  elapsed)
--- tokenize.go
+++ tokenize.go
@@ -3,10 +3,43 @@ package glub
 import (
 	"text/scanner"
 	"strings"
+	"sort"
 )
 
-func GenTokens(inputset []string) []map[string]int {
-	dataset := make([]map[string]int, len(inputset))
+func SortByVal(wordFrequencies map[string]int) PairList{
+  pl := make(PairList, len(wordFrequencies))
+  i := 0
+  for k, v := range wordFrequencies {
+    pl[i] = Pair{k, v}
+    i++
+  }
+  sort.Sort(sort.Reverse(pl))
+  return pl
+}
+
+/*overriding sort*/
+type Pair struct {
+  Key string
+  Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int {
+	return len(p) 
+}
+
+func (p PairList) Less(i, j int) bool {
+	return p[i].Value < p[j].Value
+}
+
+func (p PairList) Swap(i, j int){
+	p[i], p[j] = p[j], p[i]
+}
+
+func GenTokens(inputset []string) (map[string]int, []map[string]int) {
+	var dataset []map[string]int
+	allTokens := make(map[string]int)
 	for _, emails := range inputset{
 		tokens := make(map[string]int)
 		reader := strings.NewReader(emails)
@@ -14,11 +47,29 @@ func GenTokens(inputset []string) []map[string]int {
 		scn.Init(reader)
 		tok := scn.Scan()
 		tokens[scn.TokenText()]++ 
+		allTokens[scn.TokenText()] = 0
 		for tok != scanner.EOF{
-				tok = scn.Scan()
-				tokens[scn.TokenText()]++
+			tok = scn.Scan()
+			tokens[scn.TokenText()]++
+			allTokens[scn.TokenText()] = 0
 		}
 		dataset = append(dataset,tokens)
 	}
-	return dataset	
+	return allTokens, dataset	
+}
+
+func Prep(tokens map[string]int, metadata []map[string]int) [][]int{
+	var allWords [][]int
+	for word , _ := range tokens{
+		var wordSet []int 
+		for _ , stuff := range metadata{
+			if stuff[word] != 0{
+				wordSet = append(wordSet, stuff[word])	
+			}else{
+				wordSet = append(wordSet, 0)
+			}
+		}
+		allWords = append(allWords, wordSet)
+	}
+	return allWords
 }
